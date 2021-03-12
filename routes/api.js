bf9c7bbbafdf7bd11eacb01bfd5770c30e00e3d5
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-12 16:41:21 +0000

    fix: corrected pseduo code for api.js

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,20 @@
-//require in the router
-const router = require("express").Router();
-//require in the path npm package
-
-//get require which is going to return the exercise.html when the user visits /exercise
-
-//get request which is going to return the stats.html when the user visits /stats
-
-//export the router
+//require in the relevant model
+const Workout = require("../models/workout.js");
+//require in expressrouter
+const router = require("express").Router()
+//post request to create workouts
+router.post("/api/workout", ({body}, res) => {
+ Workout.create(body)
+ .then(dbWorkout => {
+     res.json(dbWorkout);
+ })
+ .catch(err => {
+     res.status(400).json(err);
+ });
+});
+//put request to update a workout
+//get request to get workouts out of database
+//get request to get a range of workouts out of the database
+//dete request to delete a workout
+//exports the router
 module.exports = router;
