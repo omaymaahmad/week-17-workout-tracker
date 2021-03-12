d492d332eddc65657f9ea9b001648c86dc126ba6
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-12 16:27:16 +0000

    feat: view.js file

--- routes/view.js
+++ routes/view.js
@@ -1,7 +1,14 @@
 //require in the router
 const router = require("express").Router();
 //require in the path npm package
+const router = require("mongoose").Router();
 //get require which is going to return the exercise.html when the user visits /exercise
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '../public/exercise.html'))
+);
 //get request which is going to return the stats.html when the user visits /stats
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/stats.html'))
+);
 //export the router
 module.exports = router;
